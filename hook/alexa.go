075b8750ab661bbc0c2d8e790d6e4403d7ce8163
Hakkin Lain <hakkin@github>
2018-08-08 10:11:45 -0700

    Send youtube videos as voice message.

---
+++ hook/alexa.go
@@ -0,0 +1,57 @@
+package hook
+
+import (
+	"os/exec"
+	"strings"
+
+	"github.com/NANNERPISS/NANNERPISS/context"
+	"gopkg.in/telegram-bot-api.v4"
+)
+
+func init() {
+	Register("alexa", Alexa)
+}
+
+func Alexa(ctx *context.Context, message *tgbotapi.Message) error {
+	prefix := "alexa, play "
+	if !strings.HasPrefix(strings.ToLower(message.Text), prefix) {
+		return nil
+	}
+
+	ytdlCmd := exec.Command("youtube-dl",
+		"-f", "bestaudio[filesize<=5242880]",
+		"-o", "-",
+		"ytsearch:"+message.Text[len(prefix):])
+
+	ytdlOut, err := ytdlCmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	ffCmd := exec.Command("ffmpeg",
+		"-i", "-",
+		"-c", "none",
+		"-c:a", "libvorbis",
+		"-b:a", "48k",
+		"-f", "ogg",
+		"-",
+	)
+
+	ffCmd.Stdin = ytdlOut
+	ffOut, err := ffCmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	if err = ytdlCmd.Start(); err != nil {
+		return err
+	}
+	if err = ffCmd.Start(); err != nil {
+		return err
+	}
+
+	vc := tgbotapi.NewVoiceUpload(message.Chat.ID, tgbotapi.FileReader{Name: "alexa.ogg", Reader: ffOut, Size: -1})
+	vc.BaseFile.BaseChat.ReplyToMessageID = message.MessageID
+	_, err = ctx.TG.Send(vc)
+	return err
+}
